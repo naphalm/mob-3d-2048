81a0481991cb3df3434562bd485bb8523e61c9a9
naphalm <67642330+naphalm@users.noreply.github.com>
2024-03-06 10:17:19 +0200

    Update ScreenSizeHandle.cs

--- Assets/Scripts/0 Interfaces/ScreenSizeHandle.cs
+++ Assets/Scripts/0 Interfaces/ScreenSizeHandle.cs
@@ -7,18 +7,20 @@ public class ScreenSizeHandle : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        EventRelay.Screen.LandscapeMode.AddListener(OnLandscapeMode);
         EventRelay.Screen.PortraitMode.AddListener(OnPortraitMode);
+        EventRelay.Screen.LandscapeMode.AddListener(OnLandscapeMode);
     }
 
     protected virtual void OnPortraitMode()
     {
         transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-        // BoardManager.Instance.
+        EventRelay.Screen.PortraitMode.RemoveListener(OnPortraitMode);
     }
 
     protected virtual void OnLandscapeMode()
     {
         transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+        EventRelay.Screen.LandscapeMode.RemoveListener(OnLandscapeMode);
+
     }
 }
